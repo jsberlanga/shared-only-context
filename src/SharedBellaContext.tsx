6443cc8eb5f3fd92cf87b6bbf7d35b2d14babbb9
Julio Soto <julio.soto@schibsted.com>
2021-06-19 23:45:53 +0200

    name change

--- src/SharedBellaContext.tsx
+++ src/SharedBellaContext.tsx
@@ -4,30 +4,4 @@ const SharedBellaContext = React.createContext<{ isBellaPageType?: boolean }>({
   isBellaPageType: undefined,
 });
 
-interface SharedBellaContextProviderProps {
-  pageType: string;
-  children?: React.ReactNode;
-}
-
-const useIsBellaPageType = (pageType: string) => pageType === "/bella";
-
-const SharedBellaContextProvider = ({
-  pageType,
-  children,
-}: SharedBellaContextProviderProps) => {
-  const isBellaPageType = useIsBellaPageType(pageType);
-
-  return (
-    <SharedBellaContext.Provider value={{ isBellaPageType }}>
-      {children}
-    </SharedBellaContext.Provider>
-  );
-};
-
-const useSharedBellaContext = () => {
-  const context = React.useContext(SharedBellaContext);
-
-  return context;
-};
-
-export { SharedBellaContextProvider, useSharedBellaContext };
+export { SharedBellaContext };
